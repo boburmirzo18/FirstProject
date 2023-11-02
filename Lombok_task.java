919e3042ce5b29a957472551c35fe39d749f03bd
Boburmirzo <polandstudent2207@gmail.com>
2023-11-03 00:28:52 +0100

    code

---
+++ Lombok_task.java
@@ -0,0 +1,19 @@
+
+
+
+
+public class Lombok_task {
+    private String brand;
+    private  String model;
+    private int year;
+    private int price;
+    private String color;
+    private int quantity;
+
+}
+
+class CarLombook {
+    public static void main(String[] args) {
+
+    }
+}
